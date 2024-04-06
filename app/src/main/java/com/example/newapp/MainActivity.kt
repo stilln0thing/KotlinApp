2937e179930057c517b86c021126177e8b5e37d7
MachineMaven <mathcrayon12@gmail.com>
2024-04-06 11:06:57 +0530

    Adding new files of the project

---
+++ app/src/main/java/com/example/newapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.newapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnIntent = findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener{
+            //Open a new Activity
+
+            intent= Intent(applicationContext, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
